1edc0f60df34d297577bdb10304f67c8f5f0ac96
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2022-08-02 20:46:16 +0800

    Updates to deploy script.

--- deploy_to_hugo.sh
+++ deploy_to_hugo.sh
@@ -1,3 +1,4 @@
 npm run build
 cp -r build/* ~/Personal\ Website/static/dapps/solana-faucet
 cd ~/Personal\ Website/ && ./compile_on_mac.sh
+cd ~/Github/rationalash.github.io && git add --all . && git commit -m "$1" && git push origin master
